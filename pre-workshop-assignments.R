47d53c8e0caa8fc9ef4bfe71529d29b6ee67bde2
Nguyen Tien Anh Quach <tienanhquachnguyen@gmail.com>
2025-09-03 14:38:51 -0400

    Pre workshop assignments

---
+++ pre-workshop-assignments.R
@@ -0,0 +1,16 @@
+#install.packages("neonUtilities")
+#install.packages("remotes")
+#install.packages("tidyverse")
+#install.packages("vegan")
+#install.packages("shipunov")
+
+Sys.setenv(GITHUB_PAT = "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")
+
+remotes::install_github("giocomai/ganttrify", build_vignettes = F)
+
+library(neonUtilities)
+library(remotes)
+library(tidyverse)
+library(vegan)
+library(shipunov)
+library(ganttrify)
